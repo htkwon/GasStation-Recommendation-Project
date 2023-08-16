d09bdc47feac84d1eba7b578266ecd27f361164d
htkwon <a3001sd@gmail.com>
2023-08-16 14:28:34 +0900

    first commit

---
+++ src/main/java/com/example/gasstationproject/GasStationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gasstationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasStationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GasStationProjectApplication.class, args);
+    }
+
+}
